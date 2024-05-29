015f656848f81b9bcea44fcb117ea8744d2e0962
minthukha <minthukha1239788@gmail.com>
2024-05-29 16:01:19 +0630

    [ Complete ] - MyController Using DI

--- Controllers/MyController.cs
+++ Controllers/MyController.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DependencyInjectionInASP.NET_Core.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DependencyInjectionInASP.NET_Core.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/di/v1/[controller]/get")]
     [ApiController]
     public class MyController : ControllerBase
     {
+        private readonly IMyService _myService;
+
+        public MyController(IMyService myService)
+        {
+            _myService = myService;
+        }
+
+        [HttpGet("nickname")]
+        public IActionResult GetNickName()
+        {
+            var message = _myService.GetNickName();
+            return Ok(message);
+        }   
+        [HttpGet("message")]
+        public IActionResult GetMessage()
+        {
+            var message = _myService.GetMessage();
+            return Ok(message);
+        }
     }
 }
